5f1ffafb3b7ae29ac550d3bd6fc33fea87afad23
MomenTarek11 <116780849+MomenTarek11@users.noreply.github.com>
2025-01-19 15:09:44 +0200

    Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import * as AOS from 'aos';
+import { SevicesComponent } from "./components/sevices/sevices.component";
+import { SponsersComponent } from "./components/sponsers/sponsers.component";
+import { FooterComponent } from "./shared/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, HeroComponent, SevicesComponent, SponsersComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'derby_landing';
+  ngOnInit(): void {
+    AOS.init();
+  }
 }
---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {
+  isScrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.isScrolled = window.scrollY > 100;
+  }
+
+  ngOnInit() {
+    this.onWindowScroll(); // Initialize the scroll status on load
+  }
+}
